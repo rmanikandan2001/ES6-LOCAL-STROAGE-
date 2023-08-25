0bc2a036832d57dfe673873972184b96292193f6
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 00:25:11 -0700

    add

---
+++ main.js
@@ -0,0 +1,27 @@
+//LOCAL STORAGE
+
+	localStorage.setItem("username","fabevy");
+	
+		localStorage.setItem("password","mani");
+		
+		console.log(localStorage);
+		
+		
+	let local=Object.keys(localStorage);
+
+		console.log(local);
+		
+		
+		localStorage.getItem("username");
+		
+		
+		localStorage.removeItem("username");
+		
+		console.log(localStorage);
+		
+		
+		localStorage.clear();
+		
+		console.log(localStorage);
+		
+		
